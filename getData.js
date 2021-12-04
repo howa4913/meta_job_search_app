61676b4cd2940cd4b961f391565e5d1f1ceac273
Brian Howard <brianh@Brians-MacBook-Pro.local>
2021-12-03 22:13:59 -0600

    cleanup callService function

--- getData.js
+++ getData.js
@@ -5,16 +5,15 @@ function callService(){
   document.getElementById('submitSearch').addEventListener('click', function(event){
     
     //assign payload variables
-    var description = document.getElementById('description').value;
-    var location = document.getElementById('location').value;
-    var distance = document.getElementById('distance').value;
+    var payload = generatePayload();
     var maxResults = document.getElementById('results').value;
+  
 
     var req = new XMLHttpRequest();
     //asynchronous call to microservice
-    req.open("GET", serviceURL + description + '/' + location + '/' + distance, true);
-    
+    req.open("GET", serviceURL + payload[0] + '/' + payload[1] + '/' + payload[2], true);
     req.addEventListener("load", function(){
+      clearResults();
       var data = JSON.parse(req.responseText);
       for(var i = 0; i < maxResults; i++){
         createNewList(polishContent(data, i));
@@ -25,10 +24,17 @@ function callService(){
     })
 }
 
+function generatePayload(){
+  var description = document.getElementById('description').value;
+  var location = document.getElementById('location').value;
+  var distance = document.getElementById('distance').value;
+  return [description, location, distance];
+}
+
 function createBulletPt(header, val){
   var bullet = document.createElement('li');
-  bullet.textContent = header + val
-  return bullet
+  bullet.textContent = header + val;
+  return bullet;
 }
 
 function createNewList(params){
@@ -41,32 +47,18 @@ function createNewList(params){
 
 function polishContent(data, resultNumber){
   var jobTitle = data.jobs[resultNumber].title;
-  var jobDate = data.jobs[resultNumber].updated_at;
+  var jobDate = data.jobs[resultNumber].updated_at.slice(0,10);
   var jobUrl = data.jobs[resultNumber].absolute_url;
   return [jobTitle, jobDate, jobUrl];
 }
 
 
-
-
-
-
-/*
-function displayJobs(listing, val1, val2, val3){
-  var section = document.getElementById('result' + listing);
-  var list = document.createElement('ul');
-  var bullet1 = document.createElement('li');
-  var bullet2 = document.createElement('li');
-  var bullet3 = document.createElement('li');
-  section.appendChild(list);
-  bullet1.textContent = 'Title: ' + val1;
-  bullet2.textContent = 'Posted Date: ' + val2;
-  bullet3.textContent = 'URL: ' + val3;
-  section.appendChild(bullet1);
-  section.appendChild(bullet2);
-  section.appendChild(bullet3);
+function clearResults(){
+  section = document.getElementById('resultSection');
+  while (section.firstChild){
+    section.removeChild(section.firstChild);
+  }
 }
-*/
 
 document.addEventListener("DOMContentLoaded", function(){
   callService()
